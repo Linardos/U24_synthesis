e063e4bc6c9282ce0adc9f3271e6d4121da02bc4
Akis Linardos <linardos.akis@gmail.com>
2024-09-09 13:56:51 -0400

    .

--- files_for_gandlf/split_csv.py
+++ files_for_gandlf/split_csv.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 # Load the CSV file
+x = "stylegan" #vqvae, stylegan etc
 input_file = 'original_256_train_data.csv'
+input_file = f'{x}_train_data.csv'
 data = pd.read_csv(input_file)
 
 # Calculate the number of rows
@@ -16,7 +18,8 @@ train_90 = data.iloc[:split_90_index]  # First 90%
 val_90 = pd.concat([data.iloc[:split_5_index], data.iloc[-split_5_index:]])  # First 5% + Last 5% to take equal from both labels
 
 # Save the data into two CSV files
-train_90.to_csv('0.9_train_split.csv', index=False)
-val_90.to_csv('0.1_val_split_holdout.csv', index=False)
+train_90.to_csv(f'{x}_0.9_train_split.csv', index=False)
+val_90.to_csv(f'{x}_0.1_val_split_holdout.csv', index=False)
 
 print("Splitting completed.")
+
--- main_gandlf_akis.sh
+++ main_gandlf_akis.sh
@@ -2,25 +2,56 @@
 
 # continue=False # Continue or delete the parameters
 # experiment_dir="./experiments/000_holdout_sanity_original"
-experiment_dir="./experiments/001_holdout_sanity_original_512x512"
-experiment_dir="./experiments/002_holdout_original_256x256"
-experiment_dir="./experiments/003_cv_original_256x256"
-experiment_dir="./experiments/004_holdout_vqvae"
+# experiment_dir="./experiments/001_holdout_sanity_original_512x512"
+experiment_dir="./experiments/001_holdout_original_256x256"
+experiment_dir="./experiments/004_holdout_original_256x256_vgg"
+experiment_dir="./experiments/004_cv_original_256x256_vgg"
+# experiment_dir="./experiments/005_holdout_vqvae_256x256_vgg"
+# experiment_dir="./experiments/005_cv_vqvae_256x256_vgg"
+# experiment_dir="./experiments/005_holdout_vqvae_256x256_vgg"
+experiment_dir="./experiments/006_holdout_original_256x256_flexinet"
+# experiment_dir="./experiments/006_cv_original_256x256_flexinet"
+# experiment_dir="./experiments/001_cv_original_256x256"
+# experiment_dir="./experiments/002_cv_vqvae_256x256"
+# experiment_dir="./experiments/002_holdout_vqvae"
 # experiment_dir="./experiments/000_holdout_sanity_vqvae"
 # experiment_dir="./experiments/001_holdout_original"
 # experiment_dir="./experiments/002_holdout_vqvae"
 # experiment_dir="./experiments/003_holdout_stylegan"
-# train_csv="original_train_data.csv"
-train_csv='original_256_train_data.csv'
-train_csv="vqvae_train_data.csv"
-# train_csv="vqvae_train_data_subset.csv"
-# train_csv="stylegan_train_data.csv"
 
-if [[ "$experiment_dir" == *"holdout"* ]]; then
-    train_csv="0.9_train_split.csv"
-    val_csv="0.1_val_split_holdout.csv"
-    echo training set for holdout with csv files $train_csv and $val_csv
+if [[ "$experiment_dir" == *"vqvae"* ]]; then
+  if [[ "$experiment_dir" == *"holdout"* ]]; then
+      train_csv="vqvae_0.9_train_split.csv"
+      val_csv="vqvae_0.1_val_split_holdout.csv"
+      cp ./files_for_gandlf/$val_csv "$experiment_dir/"
+      echo training set for holdout with csv files $train_csv and $val_csv
+  else
+      train_csv="vqvae_train_data.csv"
+      echo training set for cv: $train_csv
+  fi
+elif [[ "$experiment_dir" == *"original"* ]]; then
+  if [[ "$experiment_dir" == *"holdout"* ]]; then
+      train_csv="0.9_train_split.csv"
+      val_csv="0.1_val_split_holdout.csv"
+      cp ./files_for_gandlf/$val_csv "$experiment_dir/"
+      echo training set for holdout with csv files $train_csv and $val_csv
+  else
+      train_csv="original_256_train_data.csv"
+      echo training set for cv: $train_csv
+  fi
+elif [[ "$experiment_dir" == *"stylegan"* ]]; then
+  if [[ "$experiment_dir" == *"holdout"* ]]; then
+      train_csv="stylegan_0.9_train_split.csv"
+      val_csv="stylegan_0.1_val_split_holdout.csv"
+      cp ./files_for_gandlf/$val_csv "$experiment_dir/"
+      echo training set for holdout with csv files $train_csv and $val_csv
+  else
+      train_csv="stylegan_train_data.csv"
+      echo training set for cv: $train_csv
+  fi
 fi
+
+export CUDA_VISIBLE_DEVICES=0
 
 # Check if the directory exists, if not create it
 if [ ! -d "$experiment_dir" ]; then
@@ -30,7 +61,7 @@ fi
 # Copy the necessary files into the holdout directory
 cp ./files_for_gandlf/gandlf_config.yaml "$experiment_dir/"
 cp ./files_for_gandlf/$train_csv "$experiment_dir/"
-# cp ./files_for_gandlf/$val_csv "$experiment_dir/"
+# 
 echo starting training for $experiment_dir
 
 # hold out experiment
@@ -51,6 +82,7 @@ else
     --train \
     -d cuda
 fi
+
 # hold out experiment with the same val and train test for sanity checks
 # gandlf run \
 #   -c "$experiment_dir/gandlf_config.yaml" \
